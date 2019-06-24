5e343e80575666f550869fff2e3980d806dd6e88
Andre Queiroz <andre.queiroz.jrvs.ca@gmail.com>
2019-06-24 19:15:56 +0000

    add parameters to host_info.sh

--- scripts/host_info.sh
+++ scripts/host_info.sh
@@ -1,10 +1,21 @@
 #! /bin/bash
 
 #Database Connection
-DATABASE=host_agent  
-USERNAME=postgres
-HOST=localhost
-export PGPASSWORD=pg_db_password
+if [ -z $1]
+then
+  echo "No parameters were passed. Using default values"
+  HOST=localhost
+  DATABASE=host_agent  
+  USERNAME=postgres 
+  export PGPASSWORD=password
+else
+  echo "Using parameters passed by the user: HOST=${1} , USERNAME=${2}, DATABASE=${3},PASSWORD=***"
+  HOST=$1
+  USERNAME=$2
+  DATABASE=$3
+  export PGPASSWORD=$4
+fi
+
 
 # CPU usage information
 memory_free=`vmstat --unit M | tail -1 | awk '{print $4}'`
@@ -12,7 +23,7 @@ cpu_idel=`vmstat --unit M | tail -1 | awk '{print $15}'`
 cpu_kernel=`vmstat --unit M | tail -1 | awk '{print $14}'`
 disk_io=`vmstat -d | tail -1 | awk '{print $10}'`
 disk_available=`df -BM / | tail -1 | awk '{print $4}'| sed 's/[^0-9]*//g'`
-export PGPASSWORD=password
+
 
 # Storing on database
 psql -h $HOST -U $USERNAME -d $DATABASE -p 5432 -e -v ON_ERROR_STOP=1<<EOF
